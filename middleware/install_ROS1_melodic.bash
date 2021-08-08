e43fdeecdc9d1af268372abf802d4629443a02b3
Pin Loon Lee <pinloon_0428@hotmail.com>
2021-08-08 10:29:39 +0800

    middleware: update ros1 script with latest documentation

--- middleware/install_ROS1_melodic.bash
+++ middleware/install_ROS1_melodic.bash
@@ -3,17 +3,18 @@
 # install ROS melodic
 # Reference: 
 # 1. http://wiki.ros.org/melodic/Installation/Ubuntu
-# 2. https://www.theconstructsim.com/how-to-install-ros-on-ubuntu/
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt install curl # if you haven't already installed curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+
 sudo apt update
-sudo apt install -y ros-melodic-desktop-full
+sudo apt install -y ros-melodic-desktop-full    
 
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
 sudo apt install -y python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
 
-sudo apt install python-rosdep
+sudo apt install -y python-rosdep
 sudo rosdep init
 rosdep update
