1f09c117fdef20611e1b4268b05427c2e3b7cce7
Pin Loon Lee <pinloon_0428@hotmail.com>
2021-08-15 21:42:14 +0800

    add scripts for tools

---
+++ tools/install_docker_compose.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# install Docker Compose
+# Reference:
+# 1. https://docs.docker.com/compose/install/
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
