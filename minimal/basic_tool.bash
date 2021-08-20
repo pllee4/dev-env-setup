83656a8e815cd2d7a16965fa4b3f7020ab95179b
Pin Loon Lee <pinloon_0428@hotmail.com>
2021-08-20 22:11:37 +0800

    add minimal folder

---
+++ minimal/basic_tool.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt -y install curl
+sudo apt -y install htop
+sudo apt -y install terminator
+sudo apt -y install can-utils
+sudo apt -y install build-essential
+sudo apt -y install clang-format
---
+++ minimal/minimal_setup.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt -y upgrade
+sudo apt -y dist-upgrade
+
+./basic_tool.bash
