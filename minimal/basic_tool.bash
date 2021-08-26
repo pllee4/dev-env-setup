4d27a83a80e7b19d866ff61c886f431b8f4677a7
Pin Loon Lee <pinloon_0428@hotmail.com>
2021-08-26 20:45:03 +0800

    minimal: basic_tool.bash: add net-tools

--- minimal/basic_tool.bash
+++ minimal/basic_tool.bash
@@ -4,5 +4,6 @@ sudo apt -y install curl
 sudo apt -y install htop
 sudo apt -y install terminator
 sudo apt -y install can-utils
+sudo apt -y install net-tools
 sudo apt -y install build-essential
 sudo apt -y install clang-format
