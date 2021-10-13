9e99602c0c13ba5e267d53bd48ebeffd426848be
Pin Loon Lee <pinloon_0428@hotmail.com>
2021-10-13 20:11:14 +0800

    middleware: add installation for ROS2 foxy

---
+++ middleware/install_ROS2_foxy.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# install ROS foxy
+# Reference: 
+# 1. https://docs.ros.org/en/foxy/Installation/Ubuntu-Development-Setup.html
+
+sudo apt update && sudo apt install -y curl gnupg2 lsb-release
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo apt update
+sudo apt install -y ros-foxy-desktop
+
+sudo sh -c 'echo "deb [arch=amd64,arm64] http://repo.ros2.org/ubuntu/main `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo apt update
+sudo apt install -y python3-colcon-common-extensions
+
+echo "source /opt/ros/foxy/setup.bash" >> ~/.bashrc
+source ~/.bashrc
