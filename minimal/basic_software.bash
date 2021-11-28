89a33ca8b51c3437f2660c92909225cd2acb9c46
Pin Loon Lee <pinloon_0428@hotmail.com>
2021-11-28 13:50:57 +0800

    minimal: add basic_software.bash

---
+++ minimal/basic_software.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Google Chrome
+sudo apt-get -y install wget
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
