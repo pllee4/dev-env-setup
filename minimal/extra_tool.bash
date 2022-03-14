1753454cc3da006fc41d1dfab4431a752d603921
Pin Loon Lee <pinloon_0428@hotmail.com>
2022-03-14 22:47:41 +0800

    minimal: extra_tool.bash: added installation for nmap

--- minimal/extra_tool.bash
+++ minimal/extra_tool.bash
@@ -5,3 +5,7 @@ sudo apt -y install openssh-server
 sudo apt -y install sshpass
 
 # sshpass -p "$password" scp /<PATH>/final.txt $username@$Ip:/root/<PATH>
+
+sudo apt -y install nmap
+
+# sudo nmap -sn <ip_address>/24 // for example 192.168.10.0/24
